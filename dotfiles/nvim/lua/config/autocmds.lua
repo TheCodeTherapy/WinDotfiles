1d247c770562204b4633491cffcf6532c6bae879
TheCodeTherapy <me@mgz.me>
2025-09-14 02:28:23 +0100

    updates nvim configs

--- dotfiles/nvim/lua/config/autocmds.lua
+++ dotfiles/nvim/lua/config/autocmds.lua
@@ -1,25 +1,3 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
---
-
-vim.cmd([[
-  autocmd ColorScheme * highlight Normal guibg=NONE ctermbg=NONE
-  autocmd ColorScheme * highlight NormalFloat guibg=NONE ctermbg=NONE
-  autocmd ColorScheme * highlight SignColumn guibg=NONE ctermbg=NONE
-]])
-
-vim.cmd([[
-  highlight Normal guibg=NONE ctermbg=NONE
-  highlight NormalFloat guibg=NONE ctermbg=NONE
-  highlight SignColumn guibg=NONE ctermbg=NONE
-]])
-
 vim.o.updatetime = 300
 
 local function smart_hover()
--- dotfiles/nvim/lua/plugins/markdown.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "iamcco/markdown-preview.nvim",
-  config = function()
-    -- vim.fn["mkdp#util#install"]()
-    if not vim.fn.exists("g:mkdp_path") then
-      vim.fn["mkdp#util#install"]()
-    end
-  end,
-}
